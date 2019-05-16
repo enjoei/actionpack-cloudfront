7442df90be13d051776eab66af9fb1c6aac457c2
Thiago Tonon <thiagotonon2@gmail.com>
2019-05-16 19:36:15 -0300

    Add incapsula proxies and use all ips from AWS

--- lib/action_pack/cloudfront/ip_ranges.rb
+++ lib/action_pack/cloudfront/ip_ranges.rb
@@ -3,16 +3,11 @@ module ActionPack
     module IpRanges
 
       class Range
-        attr_reader :ip_prefix, :region, :service
+        attr_reader :ip_prefix, :region
 
         def initialize(attrs)
-          @service = attrs['service']
           @ip_prefix = attrs['ip_prefix'] || attrs['ipv6_prefix']
           @region = attrs['region']
-        end
-
-        def cloudfront?
-          service =~ /cloudfront/i
         end
 
         def ipaddr
@@ -21,11 +16,21 @@ module ActionPack
       end
 
       def trusted_proxies
-        cloudfront_proxies + ActionDispatch::RemoteIp::TRUSTED_PROXIES
+        incapsula_proxies + aws_proxies + ActionDispatch::RemoteIp::TRUSTED_PROXIES
       end
 
-      def cloudfront_proxies
-        ip_ranges.select(&:cloudfront?).map(&:ipaddr)
+      def incapsula_proxies
+        proxies = [
+          '199.83.128.0/21', '198.143.32.0/19', '149.126.72.0/21', '103.28.248.0/22', '45.64.64.0/22',
+          '185.11.124.0/22', '192.230.64.0/18', '107.154.0.0/16', '45.60.0.0/16', '45.223.0.0/16',
+          '2a02:e980::/29'
+        ]
+
+        proxies.map { |ip_prefix| IPAddr.new(ip_prefix) }
+      end
+
+      def aws_proxies
+        ip_ranges.map(&:ipaddr)
       end
 
       def ip_ranges
